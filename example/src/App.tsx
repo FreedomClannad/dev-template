886fc5f973dbafe5492d124df3c21f2ba2f8e230
freedom <459566948@qq.com>
2024-08-15 14:43:04 +0800

    组件开发流程测试编写

---
+++ example/src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+import { MolSVG } from "@am/ketcher";
+function App() {
+	const [textarea, setTextarea] = useState<string>("");
+	const [mol, setMol] = useState<string>(`Benzene
+   Ketcher  8152413542D 1   1.00000     0.00000     0
+
+ 12 13  0  0  0  0  0  0  0  0999 V2000
+    7.3848   -1.7751    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    9.1152   -1.7746    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    8.2516   -1.2750    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    9.1152   -2.7755    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    7.3848   -2.7800    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    8.2538   -3.2750    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    9.9812   -3.2755    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+   11.7115   -3.2739    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+   10.8476   -2.7748    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+   11.7121   -4.2748    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    9.9819   -4.2805    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+   10.8512   -4.7749    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+  3  1  2  0     0  0
+  1  5  1  0     0  0
+  5  6  2  0     0  0
+  6  4  1  0     0  0
+  4  2  2  0     0  0
+  2  3  1  0     0  0
+  4  7  1  0     0  0
+  9  7  2  0     0  0
+  7 11  1  0     0  0
+ 11 12  2  0     0  0
+ 12 10  1  0     0  0
+ 10  8  2  0     0  0
+  8  9  1  0     0  0
+M  END`);
+
+	const handleClick = () => {
+		setMol(`Benzene
+   ${textarea}`);
+	};
+
+	return (
+		<>
+			<textarea
+				onChange={e => {
+					setTextarea(e.target.value);
+				}}
+			></textarea>
+			<button onClick={handleClick}>生成</button>
+			<div>
+				<MolSVG mol={mol}></MolSVG>
+			</div>
+		</>
+	);
+}
+
+export default App;
